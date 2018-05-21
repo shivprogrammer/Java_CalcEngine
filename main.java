315f62a70188723cec2296bacb9e7f248aaee231
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-20 21:36:44 -0700

    basic variables in Java

--- main.java
+++ main.java
@@ -4,3 +4,8 @@ public class Main {
     System.out.println("Hello World");
   }
 }
+
+int dataValue;
+dataValue = 100; // you can declare a variable and define the value of it separately
+
+int myInfo = 200; // or you can do both declare and define on the same line
