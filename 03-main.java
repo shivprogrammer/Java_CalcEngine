b822d2f5d9d58d2c8335ba652eaa5b6015dd6ca0
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-22 00:02:46 -0700

    solidifying order of operations outcomes

--- 03-main.java
+++ 03-main.java
@@ -7,5 +7,12 @@ public class Main {
     int valB = 6;
     int valC = 3;
     int valD = 1;
+
+    int result1 = valA - valB / valC;
+
+    int result2 = (valA - B) / valC;
+
+    System.out.println(result1); // returns 19
+    System.out.println(result2); // returns 5
   }
 }
