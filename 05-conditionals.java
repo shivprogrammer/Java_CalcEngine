3fb12381230d74fb86877f3e46b89e99d1f4b7f7
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-25 22:27:32 -0700

    more conditional work

--- 05-conditionals.java
+++ 05-conditionals.java
@@ -3,3 +3,9 @@ int v2 = 5;
 
 int vMax = v1 > v2 ? v1 : v2;
 System.out.println(vMax); // returns 7
+
+float students = 30;
+float rooms = 4;
+
+float studentsPerRoom = rooms = 0.0f : students/rooms
+System.out.println(studentsPerRoom); // returns 7.5
