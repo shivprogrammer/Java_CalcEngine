2849b6bb94957ce96bfa1e25a17ba2b770615971
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-25 22:51:03 -0700

    getting some work in with nested ifs in java

---
+++ 08-nested_ifs.java
@@ -0,0 +1,12 @@
+package com.pluralsight.nestedits;
+
+public class Main {
+
+  public static void main(String[] args) {
+    float student = 0.0f;
+    float rooms = 4.0f;
+
+    System.out.println();
+    System.out.println("** end program **");
+  }
+}
