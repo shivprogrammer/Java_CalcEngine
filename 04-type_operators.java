35b216e7260c42ea21f4665dcc914ff109042f5f
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-25 21:20:35 -0700

    going HAM on these type conversions

--- 04-type_operators.java
+++ 04-type_operators.java
@@ -14,11 +14,13 @@ public class Main {
     short result1 = (short) longVal; // success
     short result1 = (short) byteVal; // success, but is unnecessary
 
-    short result2 = byteVal - longVal; // this will not work because you cannot convert a long to a short
-    short result2 = (short) (byteVal - longVal); // this works
+    short result2 = byteVal - longVal; // error, you cannot convert a long to a short
+    short result2 = (short) (byteVal - longVal); // success
 
     long result3 = longVal - floatVal; // error, incompatible types: possible lost conversion from float to long
     float result3 = longVal - floatVal; // success
+    float result3 = longVal - doubleVal; // error, incompatible types: possible lossy conversion from double to float
+    double result3 = longVal - doubleVal; // success
 
     System.out.println("Success");
   }
