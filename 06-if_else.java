5f4c9e099c1c89de0733c1f1a0adfa1b627b1281
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-25 22:36:52 -0700

    conditions with else if

--- 06-if_else.java
+++ 06-if_else.java
@@ -5,3 +5,13 @@ if (v1 > v2)
   System.out.println("v1 is bigger");
 else
   System.out.println("v1 is NOT bigger");
+
+int v1 = 10;
+int v2 = 40;
+
+if (v2 > v1)
+  System.out.println("v1 is bigger");
+else if (v1 < v2)
+  System.out.println("v2 is bigger");
+else
+  System.out.println("v1 and v2 are equal");
