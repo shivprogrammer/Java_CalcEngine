173cc53b74bec1f9e132d8bb0c5f7d01ec32477b
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-25 21:05:52 -0700

    solidfying type conversion concepts

--- 04-type_operators.java
+++ 04-type_operators.java
@@ -8,5 +8,11 @@ public class Main {
     byte byteVal = 7;
     short shortVal = 7;
     long longVal = 5;
+
+    short result1 = byteVal; // this works
+    short result1 = longVal; // this will not work due to type conversion clash
+    short result1 = (short) longVal; // this will work
+
+    System.out.println("Success");
   }
 }
