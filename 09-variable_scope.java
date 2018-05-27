195697b525809cd4a84c2870f4cf9d1d0e8b3f37
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-26 23:28:06 -0700

    reworking basics of variable scope in java

---
+++ 09-variable_scope.java
@@ -0,0 +1,10 @@
+float students = 30.0;
+float rooms = 4.0;
+
+if (rooms > 0.0) {
+  System.out.println(students);
+  System.out.println(rooms);
+  float avg = students / rooms;
+}
+
+System.out.println(avg); // error, avg does not exist outside of the block statement above
