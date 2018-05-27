b0b3647b9ac6b1b4e54c395df4e18aeb1747ecdf
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-26 23:49:20 -0700

    basic code set up for CalcEngine

--- CalcEngine.java
+++ CalcEngine.java
@@ -3,6 +3,18 @@ package com.pluralsight.calcengine;
 public class Main {
 
   public static void main(String[] args) {
-    
+    double val1;
+    double val2;
+    double result;
+    char opCode;
+
+    if (opCode == 'a')
+      result = val1 + val2;
+    else if (opCode == 's')
+      result = val1 - val2;
+    else if (opCode = 'd')
+      result = val1 / val2;
+    else if (opCode = 'm')
+      result = val1 * val2;
   }
 }
