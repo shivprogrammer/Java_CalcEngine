3632b05659374ef00b0c7b080130281a8a0bdad9
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-26 23:44:06 -0700

    establishing difference in logical vs conditional logical and operator

--- 11-logical_vs_conditional_logical_and.java
+++ 11-logical_vs_conditional_logical_and.java
@@ -6,6 +6,10 @@ public class Main {
     int students = 150;
     int rooms = 0;
 
+    if (rooms > 0 & students/rooms > 30) // so you get an error saying that you can't divide by 0
+    if (rooms > 0 && students/rooms > 30) // success
+      System.out.println("Crowded!!");
+
     System.out.println();
     System.out.println("** end progress ** ");
   }
