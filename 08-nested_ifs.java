938a424f4e14e8139c47d929e965e3217e2ba711
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-26 23:23:09 -0700

    some nested ifs practice in java

--- 08-nested_ifs.java
+++ 08-nested_ifs.java
@@ -6,6 +6,11 @@ public class Main {
     float student = 0.0f;
     float rooms = 4.0f;
 
+    if (students > 0.0f)
+      System.out.println(students/rooms);
+    else
+      System.out.println("No students");
+
     System.out.println();
     System.out.println("** end program **");
   }
