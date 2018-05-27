c771d9c2f2b929ceb00db0b53e39669b473eea41
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-26 23:29:16 -0700

    scope review success

--- 09-variable_scope.java
+++ 09-variable_scope.java
@@ -5,6 +5,7 @@ if (rooms > 0.0) {
   System.out.println(students);
   System.out.println(rooms);
   float avg = students / rooms;
+  System.out.println(avg); // success
 }
 
 System.out.println(avg); // error, avg does not exist outside of the block statement above
