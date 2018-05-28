6d082159c894dd8182092fe29a50e01c7e6f210c
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-28 00:27:20 -0700

    CalcEngine refactored with arrays for all variables

--- CalcEngine.java
+++ CalcEngine.java
@@ -5,10 +5,8 @@ public class Main {
   public static void main(String[] args) {
     double[] leftVals = { 100.0d, 25.0d, 225.0d, 11.0d };
     double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
-    double val1 = 100.0d;
-    double val2 = 50.0d;
-    double result;
-    char opCode = 'a';
+    char[] opCodes = { 'd', 'a', 's', 'm' };
+    double[] results = new double[opCodes.length];
 
     if (opCode == 'a')
       result = val1 + val2;
