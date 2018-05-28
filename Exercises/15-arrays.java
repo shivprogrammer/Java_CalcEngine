adb69084ac0262fdf40ea0f5aa1e17c27052814f
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-27 23:57:42 -0700

    for loop run through of an array in java

--- Exercises/15-arrays.java
+++ Exercises/15-arrays.java
@@ -2,3 +2,10 @@ float[] theVals = new float[3]; // this is an array of 3 float values
 theVals[0] = 10.0f;
 theVals[1] = 20.0f;
 theVals[2] = 15.0f;
+
+float sum = 0.0f;
+
+for (int i = 0; theVals.length; i++)
+  sum += theVals[i];
+
+System.out.println(sum);
