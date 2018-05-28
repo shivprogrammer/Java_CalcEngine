0e22090252c592d9e6d606be62ebf2d7875b4803
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-27 23:12:27 -0700

    CalcEngine typo correction

--- CalcEngine.java
+++ CalcEngine.java
@@ -3,10 +3,10 @@ package com.pluralsight.calcengine;
 public class Main {
 
   public static void main(String[] args) {
-    double val1;
-    double val2;
+    double val1 = 100.0d;
+    double val2 = 50.0d;
     double result;
-    char opCode;
+    char opCode = 'a';
 
     if (opCode == 'a')
       result = val1 + val2;
@@ -16,5 +16,11 @@ public class Main {
       result = val1 / val2;
     else if (opCode = 'm')
       result = val1 * val2;
+    else {
+      System.out.println("Error - invalid opCode");
+      result = 0.0d;
+    }
+
+    System.out.println(result);
   }
 }
