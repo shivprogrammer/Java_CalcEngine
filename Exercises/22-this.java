7d73249641dc35b6f203fbffda680793c6183de8
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 15:00:34 -0700

    partially rewritten Flight class in preparation for adding this keyword reference

---
+++ Exercises/22-this.java
@@ -0,0 +1,9 @@
+public class Flight {
+  private int passengers;
+  private int seats;
+
+  public boolean hasRoom(Flight f2) {
+    int total = passengers + f2.passengers;
+    return total <= seats;
+  }
+}
