74f75839438e886a040570d0a0cc9613de530c0a
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 00:39:45 -0700

    adding failsafe to showSum

--- Exercises/20-methods.java
+++ Exercises/20-methods.java
@@ -1,8 +1,10 @@
 public class MyClass {
   public void showSum (float x, float y, int count) {
+    if (count < 1)
+      return;
     float sum = x + y;
     for (int i = 0; i < count; i++)
-    System.out.println(sum);
+      System.out.println(sum);
   }
 }
 
