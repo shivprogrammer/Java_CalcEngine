acf3ae44ff3c11d5901c973e223fb4e273307122
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 15:21:49 -0700

    original version of CalcEngine rewritten and newer vesion copied into new file for further modifications

--- CalcEngine/01-CalcEngine.java
+++ CalcEngine/01-CalcEngine.java
@@ -12,10 +12,11 @@ public class Main {
     else if (opCode == 's')
       result = val1 - val2;
     else if (opCode == 'd')
-      result = val1 / val2;
+      result = val2 != 0.0d ? val1 / val2 : 0.0d;
     else if (opCode == 'm')
       result = val1 * val2;
     else
+      System.out.println("Error - invalid opCode");
       result == 0.0d;
 
     System.out.println(result);
