2a8f8e9d0f4325737f780ccaf22756c307e61fc6
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 00:20:35 -0700

    adding access modifiers to Flight class

--- Exercises/18-classes.java
+++ Exercises/18-classes.java
@@ -1,19 +1,19 @@
-class Flight {
-  int passengers;
-  int seats;
+public class Flight {
+  private int passengers;
+  private int seats;
 
-  Flight() {
+  public Flight() {
     seats = 150;
     passengers = 0;
   }
 
-  void addPassenger() {
+  public void addPassenger() {
     if (passengers < seats)
       passengers += 1;
     else
       handleTooMany();
   }
-  void handleTooMany() {
+  private void handleTooMany() {
     System.out.println("Too many");
   }
 }
@@ -29,11 +29,14 @@ Flight flight2 = new Flight();
 
 flight2.addPassenger();
 System.out.println(flight2.passengers); // returns 1
+// after adding private access modifier to passengers, this would no longer be allowed and would produce a compile time error
 
 flight2 = flight1;
 System.out.println(flight2.passengers) // returns 0
+// after adding private access modifier to passengers, this would no longer be allowed and would produce a compile time error
 
 flight1.addPassenger();
 flight1.addPassenger();
 
 System.out.println(flight2.passengers) // RETURNS 2, NOTICE THAT ASSIGNMENTS ARE POINTING TO THE SAME OBJECT INSTANCE, INSTEAD OF CREATING A NEW COPY LIKE IN PRIMITIVE TYPES
+// after adding private access modifier to passengers, this would no longer be allowed and would produce a compile time error
