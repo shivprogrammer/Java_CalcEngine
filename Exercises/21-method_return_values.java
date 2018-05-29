ee6f74b23202eb521c9e68c6cb45fdaa077f0740
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 14:52:43 -0700

    created createNewWithBoth method in flight class

--- Exercises/21-method_return_values.java
+++ Exercises/21-method_return_values.java
@@ -4,9 +4,13 @@ public class Flight {
 
   public boolean hasRoom(Flight f2) {
     int total = passengers + f2.passengers;
-    if (total <= seats)
-      return true;
-    else
-      return false
+    return total <= seats;
+  }
+
+  public Flight createNewWithBoth(Fight f2) { // notice that this method returns a Flight object
+    Flight newFlight = new Flight();
+    newFlight.seats = seats;
+    newFlight.passengers = passengers + f2.passengers;
+    return newFlight;
   }
 }
