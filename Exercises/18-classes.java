457a4dbee59f7328fc88e496545af94a916d1ddd
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 00:09:32 -0700

    working with the nuances of class instantiations

--- Exercises/18-classes.java
+++ Exercises/18-classes.java
@@ -15,6 +15,20 @@ class Flight {
 
 Flight nycToSf;
 nycToSf = new Flight();
-Flight slcToDallas = new Flight();
 
+Flight slcToDallas = new Flight();
 slcToDallas.addPassenger();
+
+Flight flight1 = new Flight();
+Flight flight2 = new Flight();
+
+flight2.addPassenger();
+System.out.println(flight2.passengers); // returns 1
+
+flight2 = flight1;
+System.out.println(flight2.passengers) // returns 0
+
+flight1.addPassenger();
+flight1.addPassenger();
+
+System.out.println(flight2.passengers) // RETURNS 2, NOTICE THAT ASSIGNMENTS ARE POINTING TO THE SAME OBJECT INSTANCE, INSTEAD OF CREATING A NEW COPY LIKE IN PRIMITIVE TYPES
