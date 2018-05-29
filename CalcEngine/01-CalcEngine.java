f6211e4292cb9682a9d1cfdb3c2a46f38f254444
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 15:14:29 -0700

    rewritting code for first CalcEngine version to showcase progression

---
+++ CalcEngine/01-CalcEngine.java
@@ -0,0 +1,10 @@
+package com.pluralsight.calcengine;
+
+public class Main {
+  public static void main(String[] args) {
+    double val1;
+    double val2;
+    double result
+    char opCode;
+  }
+}
