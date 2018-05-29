b007cacb9cde3f187b4173c0f12694694f54bda8
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 14:30:38 -0700

    added hasRoom method

--- Exercises/21-method_return_values.java
+++ Exercises/21-method_return_values.java
@@ -3,6 +3,10 @@ public class Flight {
   private int seats;
 
   public boolean hasRoom(Flight f2) {
-    
+    int total = passengers + f2.passengers;
+    if (total <= seats)
+      return true;
+    else
+      return false
   }
 }
