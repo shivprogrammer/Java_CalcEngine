740742fbf567461cc02e8fe7da30e5a770a283d9
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-29 15:17:40 -0700

    continued rewriting of first version of CalcEngine

--- CalcEngine/01-CalcEngine.java
+++ CalcEngine/01-CalcEngine.java
@@ -2,9 +2,22 @@ package com.pluralsight.calcengine;
 
 public class Main {
   public static void main(String[] args) {
-    double val1;
-    double val2;
-    double result
-    char opCode;
+    double val1 = 100.0d;
+    double val2 = 50.0d;
+    double result;
+    char opCode = 'd';
+
+    if (opCode == 'a')
+      result = val1 + val2;
+    else if (opCode == 's')
+      result = val1 - val2;
+    else if (opCode == 'd')
+      result = val1 / val2;
+    else if (opCode == 'm')
+      result = val1 * val2;
+    else
+      result == 0.0d;
+
+    System.out.println(result);
   }
 }
