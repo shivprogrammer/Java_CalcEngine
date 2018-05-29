9b4557b5ff3bae7a38544729d21bd5dc4124aa09
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-28 23:58:31 -0700

    added Flight constructor to Flight class

--- Exercises/18-classes.java
+++ Exercises/18-classes.java
@@ -2,6 +2,11 @@ class Flight {
   int passengers;
   int seats;
 
+  Flight() {
+    seats = 150;
+    passengers = 0;
+  }
+
   void addPassenger() {
     if (passengers < seats)
       passengers += 1;
