c9539fff6fc8e2f7e2eada261640560030e233f7
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-31 16:25:32 -0700

    working with parameter immutability for primitive types

---
+++ Exercises/30-parameter_immutability_primitive_types.java
@@ -0,0 +1,11 @@
+void swap(int i, int j) {
+  int k = i; // k obviously gets the value for i
+  i = j;
+  j = k;
+  // print i & j
+} // all of the changes that are happening to the values are only occuring to the copies, NOT the original values
+
+int val = 10;
+int val2 = 20;
+
+swap(val1, val2); // when you call this, memory for the parameter i is allocated, and the value of val1 is copied down into that memory for i. Same thing for j, the value for val2 gets copied down into the memory allocated for j
