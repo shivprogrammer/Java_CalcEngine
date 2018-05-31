0fe74d46520a24dd0013e072b4d46c36c9da987c
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-31 16:32:42 -0700

    continuing work with hammering the nunances of class parameter immutability

--- Exercises/31-parameter_immutability_classes.java
+++ Exercises/31-parameter_immutability_classes.java
@@ -8,3 +8,11 @@ public class Flight {
 
   // other members elided for clarity
 }
+
+void swap(Flight i, Flight j) {
+
+}
+
+Flight val1 = new Flight(10); // so here you're allocating out memory for our variable, but then you are allocating out the class instance itself separately and assign a reference into the variable
+Flight val2 = new Flight(10); // again this allocates out a place to hold the reference, the actual Flight object itself, and the associates that reference
+// NOTE val1 and val2 are NOT the flights themselves, they're references to the flights
