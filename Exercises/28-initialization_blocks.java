1eb57eb33ce5c77a79270f2843d1f2b68f4ef5ae
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-30 17:35:50 -0700

    initialization blocks added to Flight class for cleaner, more readable code

--- Exercises/28-initialization_blocks.java
+++ Exercises/28-initialization_blocks.java
@@ -1,10 +1,16 @@
 public class Flight {
   private int passengers, flightNumber, seats = 150;
   private char flightClass;
+  private boolean[] isSeatAvailble;
 
-  public Flight() {
-
+  // the brackets with nothing else preceding them turn the code inside them into an initialization block, and will automatically be executed at the beginning of each one of our constructors
+  {
+    isSeatAvailable = new Boolean[seats];
+    for (int i = 0; i < seats; i++)
+      isSeatAvailable[i] = true;
   }
+
+  public Flight() {}
 
   public Flight(int flightNumber) {
     this.flightNumber = flightNumber;
