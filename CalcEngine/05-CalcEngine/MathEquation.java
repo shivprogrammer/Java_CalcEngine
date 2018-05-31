ec8cf8ba998d7c3b437111cd2d458a1bb2c940f1
shivprogrammer <shiv.unadkat@gmail.com>
2018-05-30 17:23:16 -0700

    added varied constructors to MathEquation

--- CalcEngine/05-CalcEngine/MathEquation.java
+++ CalcEngine/05-CalcEngine/MathEquation.java
@@ -15,6 +15,18 @@ public class MathEquation {
 
   public double getResult() { return result; }
 
+  public MathEquation() {}
+
+  public MathEquation(char opCode) {
+    this.opCode = opCode;
+  }
+
+  public MathEquation(char opCode, double leftVal, double rightVal) {
+    this(opCode);
+    this.leftVal = leftVal;
+    this.rightVal = rightVal;
+  }
+
   public void execute() {
     switch (opCode) {
       case 'a':
