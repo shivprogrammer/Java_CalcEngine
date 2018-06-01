e88117e14d131cce7f12db8b92e776017fdf0d8b
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-01 14:10:11 -0700

    Utilizing all of the overload methods and their nuances

--- Exercises/33-overloading.java
+++ Exercises/33-overloading.java
@@ -41,3 +41,15 @@ public class Flight {
     return totalCarryOns + carryOns <= maxCarryOns;
   }
 }
+
+Flight f = new Flight();
+Passenger p1 = new Passenger(0,1); // 0 free checked bags, checking in 1 bag
+Passenger p2 = new Passenger(0,2);
+
+f.add1Passenger(); // this has no parameters, so obviously it's choosing the add1Passenger method that has no parameters
+f.add1Passenger(2); // There are two possible overloads that handle just one parameter, but since it's an int, the overload that handles ints will be used
+f.add1Passener(p1); // this calls the overload that accepts a passenger
+
+short threeBags = 3;
+f.add1Passener(threeBags, 2); // So there's an overload accepts 2 parameters of an int & another int, but not one that accepts a short. But the system performs an automatic type conversion because it knows how to convert a short to an int, so the int & int overload is utilized.
+f.add1Passenger(p2, 1); // Obviously, the overload with Flight and int parameters is used here
