dfe6150fc98939983d4562843391ece2837c9347
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-01 19:15:13 -0700

    Improving addPassengers method and its implementation by having a variable number of parameters be accepted within the list array parameter

--- Exercises/34-variable_number_of_parameters.java
+++ Exercises/34-variable_number_of_parameters.java
@@ -1,7 +1,7 @@
 public class Flight {
   // other members elided for clarity
 
-  public void addPassengers(Passenger[] list) {
+  public void addPassengers(Passenger... list) { // the ellipses now says that list will be a varying number of passenger references.
     if(hasSeating(list.length)) {
       passengers += list.length;
       for (Passenger passenger : list)
@@ -15,3 +15,21 @@ public class Flight {
     return passengers + count <= seats;
   }
 }
+
+// Below is a very cumbersome programmatic process
+Flight f = new Flight();
+Passenger janet = new Passenger(0,1);
+Passener john = new Passenger(0,2);
+f.addPassengers(
+  new Passenger[] { janet, john });
+
+Passenger fred = new Passenger(0,2);
+Passenger sarah = new Passenger(0,2);
+Passenger susie = new Passenger(0,0);
+
+f.addPassengers(
+  new Passenger[] { fred, sarah, susie });
+
+// AFTER changing the paramter of addPassengers from Passenger[] to Passenger... you can now add Passengers like so:
+f.addPassengers(janet, john);
+f.addPassengers(fred, sarah, susie);
