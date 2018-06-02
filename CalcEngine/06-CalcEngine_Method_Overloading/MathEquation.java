95e3074f7cf52a975449abb2caf2f201ba7eda37
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-01 18:38:16 -0700

    added new execute method overload taking in double leftVal and double rightVal parameters

--- CalcEngine/06-CalcEngine_Method_Overloading/MathEquation.java
+++ CalcEngine/06-CalcEngine_Method_Overloading/MathEquation.java
@@ -27,6 +27,13 @@ public class MathEquation {
     this.rightVal = rightVal;
   }
 
+  public void execute(double leftVal, double rightVal) {
+    this.leftVal = leftVal;
+    this.rightVal = rightVal;
+
+    execute();
+  }
+
   public void execute() {
     switch (opCode) {
       case 'a':
