b9491f541dc05d1225a29ecd96e9ee4583cb61cc
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-01 19:40:32 -0700

    created squadron array containing both Flight and CargoFlight members

--- Exercises/35-class_inheritance.java
+++ Exercises/35-class_inheritance.java
@@ -33,3 +33,11 @@ Flight f = new CargoFlight();
 Passenger jane = new Passenger(0,2);
 f.add1Passenger(jane);
 f.add1Package(1.0, 2.5, 1.5); // while a CargoFlight knows how to add a package, a Flight doesn't. So this wouldn't be a valid call, because we can only access methods that are visibile to the reference type.
+
+Flight[] squadron = new Flight[5];
+squadron[0] = new Flight();
+squadron[1] = new CargoFlight(); // notice how you can mix and match both Flight and CargoFlight in this array
+squadron[1] = new CargoFlight();
+squadron[1] = new Flight();
+squadron[1] = new CargoFlight();
+// the beauty of this is that you can loop through this doing all the Flight based stuff. For CargoFlights you may want to do some extended things but it allows us to very easily group these actions
