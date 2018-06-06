323e02f8f3837fa13be34c96b66540fd5ee33ef4
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-06 00:05:33 -0700

    overriding Object equals method to fit the purposes of the Flight class

--- Exercises/38-equality.java
+++ Exercises/38-equality.java
@@ -10,3 +10,15 @@ if (f1.equals(f2))
   // do something
 
 // this will also return FALSE. The equals operator also performs a reference equals test, in other words it does the same thing as the == operator. It only resolves to true if both references point to the exact same object instance
+
+class Flight {
+  // other members elided for clarity
+  private int flightNumber;
+  private char flightClass;
+  // we either have a flightNumber with no flightClass, or a flightClass with no flightNumber
+
+  @Override
+  public boolean equals(Object o) {
+    Flight other = (Flight) o; // Because o is declared as an Object, it will only have access to members that are known by Object, it won't have access to the Flight members. So what we need to do is take that o, cast to a Flight and put it into a reference of type Flight. So now "other" will have access to the Flight information from f2
+  }
+}
