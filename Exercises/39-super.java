f425567918b70706c1ffb1be646e751df518b4c6
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-06 00:25:17 -0700

    adding in base class implementation using super keyword in overridden Flight equals method

--- Exercises/39-super.java
+++ Exercises/39-super.java
@@ -5,6 +5,9 @@ class Flight {
 
   @Override
   public boolean equals (Object o) {
+    if (super.equals(o)) // when we call into the Flight implementation of equals, the first thing that we do is call into our base class implementation, which compares to see if both references point to the same object. If they do, we just exit out of here. Otherwise, we do the other tests we have buitl inside of our equals implementation
+      return true;
+
     if (!(o instanceOf Flight))
       return false;
 
@@ -13,3 +16,9 @@ class Flight {
       flightNumber == other.flightNumber && flightClass == flightClass;
   }
 }
+
+Flight f1 = new Flight(175);
+Flight f2 = f1; // f1 and f2 are pointing to the exact same Flight instance
+
+if (f1.equals(f2))
+  // do something
