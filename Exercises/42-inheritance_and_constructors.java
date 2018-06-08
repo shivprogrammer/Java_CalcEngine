cd60ac4a10f859f94498af99472f91784f148e28
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-07 21:07:55 -0700

    recreated part of Flight class for inheritance and constructor practice

---
+++ Exercises/42-inheritance_and_constructors.java
@@ -0,0 +1,10 @@
+public class Flight {
+  // other members elided for clarity
+  private int flightNumber;
+
+  public Flight() {}
+
+  public Flight(int flightNumber) {
+    this.flightNumber = flightNumber;
+  }
+}
