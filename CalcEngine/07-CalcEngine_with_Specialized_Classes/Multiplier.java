af180a3a4a872d26005da21329c71dc5438c9b0a
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-07 23:51:34 -0700

    creating new Multiplier class

---
+++ CalcEngine/07-CalcEngine_with_Specialized_Classes/Multiplier.java
@@ -0,0 +1,11 @@
+package com.pluralsight.calcengine;
+
+public class Multiplier extends CalculateBase {
+  public Multiplier() {}
+  public Multiplier(double leftVal, double rightVal) {
+    super(this.leftVal, this.rightVal);
+  }
+
+  @Override
+  public class calculate()
+}
