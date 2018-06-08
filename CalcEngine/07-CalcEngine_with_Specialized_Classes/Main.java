0ef098a092f852e020616e22a725e8a788b29c4f
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-08 00:02:51 -0700

    updating Main class using new CalculateBase extension methods

--- CalcEngine/07-CalcEngine_with_Specialized_Classes/Main.java
+++ CalcEngine/07-CalcEngine_with_Specialized_Classes/Main.java
@@ -1,11 +1,6 @@
 package com.pluralsight.calcengine;
 
 public class Main {
-  // MathEquation testEquation = new MathEquation();
-  // testEquation.execute();
-  // System.out.print("test=");
-  // System.out.println(testEquation.getResult());
-
   public static void main(String[] args) {
     MathEquation[] equations = new MathEquation(4);
     equations[0] = new MathEquation('d', 100.0d, 50.0d);
@@ -32,16 +27,31 @@ public class Main {
 
     equationOverload.execute(leftDouble, rightDouble);
     System.out.print("result=");
-    System.out.println(equationOverload.getResult()); // returns 2.25
+    System.out.println(equationOverload.getResult());
 
     equationOverload.execute(leftInt, rightInt);
     System.out.print("result=");
-    System.out.println(equationOverload.getResult()); // also returns 2.25
-    // the result with the int values is also 2.25, and this is due to a widening conversion (automatic type conversion). Since a double is "wider" than an int, or in other words a double can hold anything that is inside of an int, the system can go ahead an infer that conversion and go ahead and call the method that accepts doubles, even though we provided ints.
+    System.out.println(equationOverload.getResult());
 
-    // After adding execute overload method that accepts ints
-    equationOverload.execute((double)leftInt, rightInt); // so the system will actually choose the method overload that accepts doubles, because that's the only that can hold all the paremeters
+    equationOverload.execute((double)leftInt, rightInt);
     System.out.print("result=");
-    System.out.println(equationOverload.getResult()); // returns 2.25
+    System.out.println(equationOverload.getResult());
+
+    System.out.println();
+    System.out.println("Using inheritance");
+    System.out.println();
+
+    CalculateBase[] calculators = {
+        new Divider(100.0d, 50.0d),
+        new Adder(25.0d, 92.0d),
+        new Subtracter(225.0d, 17.0d),
+        new Multiplier(11.0d, 3.0d)
+    };
+
+    for (CalculateBase calculator:calculators) {
+      calculator.calculate();
+      System.out.print("result");
+      System.out.println(calculator.getResult());
+    }
   }
 }
