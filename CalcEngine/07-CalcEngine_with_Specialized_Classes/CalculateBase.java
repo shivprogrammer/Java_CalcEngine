f8775f55facdf9868c6e0845b8e2bc1adcec583b
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-07 23:39:54 -0700

    added getters and setters to CalculateBase

--- CalcEngine/07-CalcEngine_with_Specialized_Classes/CalculateBase.java
+++ CalcEngine/07-CalcEngine_with_Specialized_Classes/CalculateBase.java
@@ -5,5 +5,12 @@ public class CalculateBase {
   private double rightVal;
   private double result;
 
-  
+  public double getLeftVal() { return leftVal; }
+  public void setLeftVal(double rightVal) { this.leftVal = leftVal; }
+  public double getRightVal() { return rightVal; }
+  public void setRightVal(double rightVal) { this.rightVal = rightVal; }
+  public void getResult() { return result; }
+  public void setResult(double result) { this.result = result; }
+
+  public CalculateBase() {}
 }
