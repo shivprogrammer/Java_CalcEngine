7b6ef1ac7ed32da64e856444056417ae5145447e
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-10 00:01:43 -0700

    string object nuances with == operator

--- Exercises/44-string_equality.java
+++ Exercises/44-string_equality.java
@@ -4,5 +4,5 @@ s1 += " Java";
 String s2 = "I";
 s2 += " Love Java";
 
-if (s1 == s2) // returns FALSE
+if (s1 == s2) // returns FALSE, becasue remember that the == operator compares values based on whether they point to the exact same object instance
   // do something
