3a9acec82e734fb4b2015bc52c0098e65274e1c4
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-10 00:04:28 -0700

    commented explanations of the nuances of string object equality comparison operators

--- Exercises/44-string_equality.java
+++ Exercises/44-string_equality.java
@@ -6,3 +6,8 @@ s2 += " Love Java";
 
 if (s1 == s2) // returns FALSE, becasue remember that the == operator compares values based on whether they point to the exact same object instance
   // do something
+
+if (s1.equals(s2)) // this actually does a character by character comparison, so this returns TRUE
+  // do something
+
+// Something to consider though is that reference based comparisons are actually quite inexpensive, whereas character by character comparisons can be very expensive, particularly in very long strings
