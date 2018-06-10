b56b341db083d33f6ca1e7e2352f111761bb9ea1
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-10 00:12:51 -0700

    utilizing the benefit of the intern method

--- Exercises/44-string_equality.java
+++ Exercises/44-string_equality.java
@@ -14,3 +14,6 @@ if (s1.equals(s2)) // this actually does a character by character comparison, so
 
 String s3 = s1.intern(); // the intern method gives us a canonicalized reference of a string value. This is basically a fancy way of saying when you call the intern method on a string value it will always return back the exact same String Object for a given string value. So this will create a new object instance of "I Love Java", and s3 will be a reference to this new object instance
 String s4 = s2.intern(); // s4 will get a reference to that exact same object instance that s3 is referencing and that the first intern call created. So the intern assures that two strings with the same value will reference the exact same object.
+
+if (s3 == s4) // returns TRUE -- the benefit of using the intern method is that we can use that very inexpensive == operator to do the comparison
+  // do something
