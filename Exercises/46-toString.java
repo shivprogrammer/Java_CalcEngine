30f0ee771bfdc84a2e3eac0ed332ef88c309484e
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 00:14:01 -0700

    overriding the toString class within the Flight class to create more useful outputs

--- Exercises/46-toString.java
+++ Exercises/46-toString.java
@@ -3,3 +3,20 @@ System.out.println(
   "My flight is " + myFlight); // returns "My flight is com.pluarlsight.foo.Flight@74a14482"
   // com.pluralsight.foo.Flight is the fully qualified class name, including the package its in
   // 74a14482 are the values that are returned by the hashcode method
+
+public class Flight {
+  int flightNumber;
+  char flightClass;
+  // other members elided for clarity
+
+  @Override
+  public String toString() { // wow you can override toString? Boss
+    if (flightNumber > 0)
+      return "Flight #" + flightNumber;
+    else
+      return "Flight Class " + flightClass;
+  }
+}
+
+System.out.println(
+  "My flight is " + myFlight); // NOW you return "My Flight is Flight #175"
