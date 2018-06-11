e6ea868efc19364848a80a1c627ef4ddfffc6a52
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 00:00:00 -0700

    concatonating strings and integrers

--- Exercises/45-converting_non_strings_to_strings.java
+++ Exercises/45-converting_non_strings_to_strings.java
@@ -4,5 +4,4 @@ String sVal = String.valueOf(iVal); // sVal be the string value of "100"
 int i = 2, j = 3;
 int result = i * j;
 System.out.println(
-  
-)
+  i + " * " + j + " = " + result); // when you're adding an integer to a string, java knows that you want the string representation of i, j & result. So the result you'll have is the concatonated string of "2 * 3 = 6"
