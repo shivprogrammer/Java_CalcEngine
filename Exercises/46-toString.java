611f52535de963c210e168cd24a08bd7c80c1cb1
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 00:08:54 -0700

    strings concatonated with non string types

---
+++ Exercises/46-toString.java
@@ -0,0 +1,5 @@
+Flight myFlight = new Flight(175);
+System.out.println(
+  "My flight is " + myFlight); // returns "My flight is com.pluarlsight.foo.Flight@74a14482"
+  // com.pluralsight.foo.Flight is the fully qualified class name, including the package its in
+  // 74a14482 are the values that are returned by the hashcode method
