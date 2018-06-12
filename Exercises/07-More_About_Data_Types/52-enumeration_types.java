a2cddaed179fcb41147a2dcee8860ce958d015b3
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 23:09:16 -0700

    working with enumeration types

--- Exercises/07-More_About_Data_Types/52-enumeration_types.java
+++ Exercises/07-More_About_Data_Types/52-enumeration_types.java
@@ -1,3 +1,19 @@
 public enum FlightCrewJob {
-  
+  Pilot,
+  CoPilot,
+  FlightAttendant,
+  AirMarshal
+}
+
+public class CrewMember {
+  private FlightCrewJob job;
+  // other members elided for clarity
+
+  public CrewMember (FlightCrewJob job) {
+    this.job = job;
+  }
+
+  public void setJob(FlightCrewJob job) {
+    this.job = job;
+  }
 }
