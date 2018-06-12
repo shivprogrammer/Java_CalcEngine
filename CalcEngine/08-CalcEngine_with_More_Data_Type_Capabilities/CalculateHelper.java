078bf443abb484c226fa52c94cc612f4e0a0e766
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-12 00:11:56 -0700

    creating CaclulateHelper class to help generate output string

---
+++ CalcEngine/08-CalcEngine_with_More_Data_Type_Capabilities/CalculateHelper.java
@@ -0,0 +1,16 @@
+package com.pluralsight.calcengine;
+
+public class CalculateHelper {
+  MathCommand command;
+  double leftValue;
+  double rightValue;
+  double result;
+
+  public void process(String statement) {
+    // add 1.0 2.0
+    String[] parts = statement.split(" ");
+    String commandString = parts[0]; // add
+    leftValue = Double.parseDouble(parts[1]) // 1.0
+    rightValue = Double.parseDouble(parts[2]) // 2.0
+  }
+}
