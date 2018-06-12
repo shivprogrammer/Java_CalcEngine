c579f2ac5cc55ba290312ffb5c29c0ac0dbcbf83
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-12 00:03:46 -0700

    created String array for printing out what operations we are performing in main in a readable string format

--- CalcEngine/08-CalcEngine_with_More_Data_Type_Capabilities/Main.java
+++ CalcEngine/08-CalcEngine_with_More_Data_Type_Capabilities/Main.java
@@ -2,6 +2,16 @@ package com.pluralsight.calcengine;
 
 public class Main {
   public static void main(String[] args) {
+    // useMathEquation();
+    // useCalculatorBase();
+
+    String[] statements = {
+      "divide 100.0 50.0",    // 100.0 / 5.0 = 2.0
+      "add 25.0 92.0",        // 25.0 + 92.0 = 117.0
+      "subtract 225.0 17.0",  // 225.0 - 17.0 = 108.0
+      "multiply 11.0 3.0"     // 11.0 * 3.0 = 33.0
+    }
+
     MathEquation[] equations = new MathEquation(4);
     equations[0] = new MathEquation('d', 100.0d, 50.0d);
     equations[1] = new MathEquation('a', 25.0d, 92.0d);
