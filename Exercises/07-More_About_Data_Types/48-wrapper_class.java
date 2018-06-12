36f8630aac041ca07758c087388d12e523bfe991
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 20:34:50 -0700

    floatValue, parseDouble, and more valueOf

--- Exercises/07-More_About_Data_Types/48-wrapper_class.java
+++ Exercises/07-More_About_Data_Types/48-wrapper_class.java
@@ -5,3 +5,10 @@ Integer c = b;
 Integer d = Integer.valueOf(100); // this is the exact same thing as Integer a = 100, so we don't really need to use this ever since Java 5, but it's good to know
 int e = d.intValue();
 Integer f = Integer.valueOf(e);
+
+Float g = Float.valueOf(18.125f);
+float h = g.floatValue();
+
+String s = "87.44";
+double s1 = Double.parseDouble(s); // returns the primitive of 87.44
+Double s2 = Double.valueOf(s); // valueOf returns a reference to the wrapper class that has value 87.44 inside of it
