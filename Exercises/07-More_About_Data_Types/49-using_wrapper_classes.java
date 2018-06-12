4d54920e3a0ad09f9ac1c002a2ad5147016f005f
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 20:44:13 -0700

    Flight class recreated for using wrapper classes

--- Exercises/07-More_About_Data_Types/49-using_wrapper_classes.java
+++ Exercises/07-More_About_Data_Types/49-using_wrapper_classes.java
@@ -2,3 +2,17 @@ Object[] stuff = new Object[3];
 stuff[0] = new Flight();
 stuff[1] = new Passenger(0, 2);
 stuff[2] = 100;
+
+public class Flight {
+  int flightNumber;
+  char flightClass;
+  // other members elided for clarity
+
+  @Override
+  public string toString() {
+    if (flightNumber > 0)
+      return "Flight #" + flightNumber;
+    else
+      return "Flight Class " + flightClass;
+  }
+}
