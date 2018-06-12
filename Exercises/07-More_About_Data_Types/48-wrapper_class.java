9fc37733f1f101154cd7c4ee9cc0a7d7c9efefd6
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-11 20:16:39 -0700

    working with the Integer wrapper class

---
+++ Exercises/07-More_About_Data_Types/48-wrapper_class.java
@@ -0,0 +1,3 @@
+Integer a = 100; // Integer a means that a is a reference to an instance of the Integer wrapper class, but 100 is the primitive value 100
+int b = a; // b is a primitive type, and java will take care of getting the value out that the refernece of a is pointing to
+Integer c = b;
