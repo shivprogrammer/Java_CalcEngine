2b9e1c993e27f99f869d6b5559b2c38420599992
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 13:33:42 -0700

    modified Main class to handle exceptions, CalcEngine now accurately responding to invalid inputs in console

--- CalcEngine/09-CalcEngine_with_Exceptions/Main.java
+++ CalcEngine/09-CalcEngine_with_Exceptions/Main.java
@@ -17,8 +17,14 @@ public class Main {
 
     CalculateHelper helper = new CalculateHelper();
     for (String statement : statements) {
-      helper.process(statement);
-      System.out.println(helper);
+      try {
+        helper.process(statement);
+        System.out.println(helper);
+      } catch (InvalidStatementException e) {
+        System.out.println(e.getMessage());
+        if (e.getCause() != null)
+          System.out.println("Original exception: " + e.getCause().getMessage());
+      }
     }
 
     MathEquation[] equations = new MathEquation(4);
