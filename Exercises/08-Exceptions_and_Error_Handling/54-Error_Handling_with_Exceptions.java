e468e7aba3c280fdfb9bb6032f94eb2257603219
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 10:34:06 -0700

    rewritten fileReader program without comments to prepare for upcoming modifications

---
+++ Exercises/08-Exceptions_and_Error_Handling/54-Error_Handling_with_Exceptions.java
@@ -0,0 +1,25 @@
+// C:\Numbers.txt
+5
+12
+6
+4
+
+/////////////
+BufferedReader reader = null;
+int total = 0;
+try {
+  reader = new BufferedReader(new FileReader("C:\\Numbers.txt"));
+  String line = null;
+  while ((line = reader.readLine()) != null)
+    total += Integer.valueOf(line);
+  System.out.println(total);
+} catch(Exception e) {
+  System.out.println(e.getMessage());
+} finally(Exception e) {
+  try {
+    if (reader != null)
+      reader.close();
+  } catch(Exception e) {
+    System.out.println(e.getMessage());
+  }
+}
