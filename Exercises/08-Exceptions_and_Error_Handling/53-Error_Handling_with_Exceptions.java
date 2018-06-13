093fbb9083661c77ab93ad7888bf937b1935e968
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-12 19:53:24 -0700

    adding error handling using a try/catch/finally block in the file reader program

--- Exercises/08-Exceptions_and_Error_Handling/53-Error_Handling_with_Exceptions.java
+++ Exercises/08-Exceptions_and_Error_Handling/53-Error_Handling_with_Exceptions.java
@@ -5,11 +5,17 @@
 4
 
 ////////////////
-BufferedReader reader = null; // BufferedReader provides an efficient way to read content
+BufferedReader reader = null; // BufferedReader provides an efficient way to read content // Also, notice that we declare this before the try block, rather than declaring it inside the try block. The reason we do this is because we need to have access to the variable reader down in our finally block. If we declared reader inside our try block, we wouldn't be able to see it in the finally block
 int total = 0;
 
-reader = new BufferedReader(new FileReader("C:\\Number.txt")); // FileReader is a built in class that takes care of the details of reading content from a file, BufferedReader is something that adds some buffering on top of that to make reading the file more efficient and also provides some helper methods
-String line = null;
-while ((line = reader.readLine()) != null) // notice that we're assigning line directly in the while loop
+try {
+  reader = new BufferedReader(new FileReader("C:\\Number.txt")); // FileReader is a built in class that takes care of the details of reading content from a file, BufferedReader is something that adds some buffering on top of that to make reading the file more efficient and also provides some helper methods
+  String line = null;
+  while ((line = reader.readLine()) != null) // notice that we're assigning line directly in the while loop
   total += Integer.valueOf(line);
-System.out.println("Total: " + total);
+  System.out.println("Total: " + total);
+} catch (Exception e) {
+  System.out.println(e.getMessage());
+} finally {
+  reader.close();
+}
