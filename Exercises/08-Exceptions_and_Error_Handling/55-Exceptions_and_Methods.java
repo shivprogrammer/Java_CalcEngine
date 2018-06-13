f709252d2aace6fdc69017782c603a6e6fa5001e
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 10:53:31 -0700

    updated addPassengers method in Flight class for reading values from a file

--- Exercises/08-Exceptions_and_Error_Handling/55-Exceptions_and_Methods.java
+++ Exercises/08-Exceptions_and_Error_Handling/55-Exceptions_and_Methods.java
@@ -3,3 +3,18 @@
 4 Rodriguez
 7 Smith
 4 Sharma
+
+//////////////
+public class Flight {
+  int passengers;
+  // other members elided for clarity
+  public void addPassengers(String filename) {
+    BufferedReader reader = null;
+    reader = new BufferedReader(new FileReader(filename));
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      String[] parts = line.split(" ");
+      passengers += Integer.valueOf(parts[0]);
+    }
+  }
+}
