f609c42a16615ce070c2ff41e1e4371c9be140de
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-12 17:10:11 -0700

    implementing basic proper error handling

--- Exercises/08-Exceptions_and_Error_Handling/52-Error_Handling_with_Exceptions.java
+++ Exercises/08-Exceptions_and_Error_Handling/52-Error_Handling_with_Exceptions.java
@@ -1,5 +1,10 @@
 int i = 12;
 int j = 5;
 
-int result = i / (j - 2);
-System.out.println(result);
+try {
+  int result = i / (j - 2);
+  System.out.println(result);
+} catch (Exception e) {
+  System.out.println("Error: " + e.getMessage());
+  e.printStackTrace();
+}
