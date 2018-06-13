b93f06b12d46eab5732e27ee30ede63f732ddd17
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 13:22:41 -0700

    finished InvalidStatementException

--- CalcEngine/09-CalcEngine_with_Exceptions/InvalidStatementException.java
+++ CalcEngine/09-CalcEngine_with_Exceptions/InvalidStatementException.java
@@ -4,4 +4,8 @@ public class InvalidStatementException extends Exception {
   public InvalidStatementException(String reason, String statement) {
     super(reason + ": " + statement);
   }
+
+  public InvalidStatementException(String reason, String statement, Throwable cause) {
+    super(reason + ": " + statement, cause);
+  }
 }
