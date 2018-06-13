ca72d35c5345229c16a2ac8977fabd4691a64a36
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 13:27:50 -0700

    added exception for non-numeric data in CalculateHelper class

--- CalcEngine/09-CalcEngine_with_Exceptions/CalculateHelper.java
+++ CalcEngine/09-CalcEngine_with_Exceptions/CalculateHelper.java
@@ -18,8 +18,13 @@ public class CalculateHelper {
       throw new InvalidStatementException("Incorrect number of fields", statement);
 
     String commandString = parts[0]; // add
-    leftValue = Double.parseDouble(parts[1]) // 1.0
-    rightValue = Double.parseDouble(parts[2]) // 2.0
+
+    try {
+      leftValue = Double.parseDouble(parts[1]) // 1.0
+      rightValue = Double.parseDouble(parts[2]) // 2.0
+    } catch (NumberFormatException e) {
+      throw new InvalidStatementException("Non-numeric data", statement, e);
+    }
 
     setCommandFromString(commandString);
 
