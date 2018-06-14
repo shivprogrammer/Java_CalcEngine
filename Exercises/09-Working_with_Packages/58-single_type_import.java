bdb15bd9ac7541dc2ebfe4d71a9148c07faa2112
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 21:04:20 -0700

    using import statements for packages

--- Exercises/09-Working_with_Packages/58-single_type_import.java
+++ Exercises/09-Working_with_Packages/58-single_type_import.java
@@ -4,8 +4,20 @@ public class Flight { ... }
 package com.pluralsight.travel;
 public class Passenger { ... }
 
+///////////
 package com.xyzcompany.bar;
 public class Beer { ... }
 
 package com.xyzcompany.bar;
 public class Wine { ... }
+
+///////////
+import com.pluralsight.travel.Flight; // this tells the compiler that whenever we use the class Flight, what we're really referring to is the fully qualified class com.pluralsight.travel.Flight
+import com.xyzcompany.bar.Beer;
+import com.pluralsight.travel.Passenger;
+import com.xyzcompany.bar.wine;
+
+Flight lax175 = new Flight(175);
+Beer liteBeer = new Beer();
+Passenger jane = new Passenger();
+Wine merlot = new Wine();
