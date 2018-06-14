3be24a6276fd93cc825ce0809fec32c84cf87091
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 20:58:48 -0700

    using single type imports

---
+++ Exercises/09-Working_with_Packages/58-single_type_import.java
@@ -0,0 +1,11 @@
+package com.pluralsight.travel;
+public class Flight { ... }
+
+package com.pluralsight.travel;
+public class Passenger { ... }
+
+package com.xyzcompany.bar;
+public class Beer { ... }
+
+package com.xyzcompany.bar;
+public class Wine { ... }
