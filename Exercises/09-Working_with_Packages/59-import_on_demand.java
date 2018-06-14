c39fabd6eeb6b8da2aaff81c2a0dd0f77e22ccb6
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-13 21:11:07 -0700

    introducing potential bug with import on demand vs single type imports

--- Exercises/09-Working_with_Packages/59-import_on_demand.java
+++ Exercises/09-Working_with_Packages/59-import_on_demand.java
@@ -11,10 +11,13 @@ public class Beer { ... }
 package com.xyzcompany.bar;
 public class Wine { ... }
 
+package com.xyzcompay.bar;
+public class Flight { ... }
 ///////////
 import com.pluralsight.travel.*; // map in all the types from the com.pluralsight.travel package
 import com.xyzcompany.bar.*;
-Flight lax175 = new Flight(175);
+
+Flight lax175 = new Flight(175); // the problem with using import on demand, is that now you'll get an error if there is a class from each package that has the same name (both of the packages now have a Flight class in them), single type imports avoid this bug
 Beer liteBeer = new Beer();
 Passenger jane = new Passenger();
 Wine merlot = new Wine();
