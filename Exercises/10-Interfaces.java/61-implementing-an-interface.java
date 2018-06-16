75ccf7f1150f75bc8c29e18dc750c2fea645413b
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-16 00:48:23 -0700

    built out compareTo method for use in the Flight class

--- Exercises/10-Interfaces.java/61-implementing-an-interface.java
+++ Exercises/10-Interfaces.java/61-implementing-an-interface.java
@@ -3,6 +3,12 @@ public class Flight implements Comparable {
   private int flightTime; // minutes past midnight
 
   public int compareTo(Object o) {
-    
+    Flight f = (Flight) o;
+    if (flightTime < f.flightTime)
+      return -1;
+    else if (flightTime > f.flightTime)
+      return 1;
+    else
+      return 0;
   }
 }
