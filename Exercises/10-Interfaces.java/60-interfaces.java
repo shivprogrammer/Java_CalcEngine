4263c758f95f64c484fbe38bd09c719540f00fee
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-16 00:02:37 -0700

    workign with implementing interfaces

--- Exercises/10-Interfaces.java/60-interfaces.java
+++ Exercises/10-Interfaces.java/60-interfaces.java
@@ -1,4 +1,8 @@
-public class Passenger {
+public class Passenger implements Comparable {
+  // other members elided for clarity
   private int memberLevel; // 3 (1st priority), 2, 1
   private int memberDays;
+  public int compareTo(Object o) {
+    Passenger p = (Passenger) o;
+  }
 }
