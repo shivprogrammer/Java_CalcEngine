9a877fe476c5efe439c1f4cd2c1ccaf7c441fb77
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-16 00:37:20 -0700

    utilizing the Comparable interface in the Passenger class

--- Exercises/10-Interfaces.java/60-interfaces.java
+++ Exercises/10-Interfaces.java/60-interfaces.java
@@ -1,6 +1,6 @@
 public class Passenger implements Comparable {
   // other members elided for clarity
-  private int memberLevel; // 3 (1st priority), 2, 1
+  private int memberLevel; // 3 (1st priority, think of platinum member), 2 (gold member), 1 (silver member)
   private int memberDays;
   public int compareTo(Object o) {
     Passenger p = (Passenger) o;
