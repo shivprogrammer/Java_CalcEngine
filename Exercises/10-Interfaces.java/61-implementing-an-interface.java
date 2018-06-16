fb47050ce992059f9dfab446613ea8a3061f4c8b
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-16 00:46:38 -0700

    implementing the Comparable interface in the Flight class

---
+++ Exercises/10-Interfaces.java/61-implementing-an-interface.java
@@ -0,0 +1,8 @@
+public class Flight implements Comparable {
+  // other members elided for clarity
+  private int flightTime; // minutes past midnight
+
+  public int compareTo(Object o) {
+    
+  }
+}
