0c15bbd5cff725e36f3ca93e45ef62b5471ec697
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-17 03:19:41 -0700

    written out compareTo method for Flight class

--- Exercises/10-Interfaces.java/62-implementing-generic-interface.java
+++ Exercises/10-Interfaces.java/62-implementing-generic-interface.java
@@ -4,4 +4,10 @@ public interface Comparable<T> { // <T> is saying that the Comparable interface 
 
 public class Flight implements Comparable {
   // other members elided for clarity
+  private int flightTime; // minutes past midnight
+
+  public int compareTo(Object o) {
+    Flight f = (Flight) o;
+    return flightTime - f.flightTime;
+  }
 }
