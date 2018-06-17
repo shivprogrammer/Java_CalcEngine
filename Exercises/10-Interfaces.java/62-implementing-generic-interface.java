dfbec3a5e5277b63bf7f0fd9badadecad71e39a2
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-17 03:21:02 -0700

    optimizing the Flight class

--- Exercises/10-Interfaces.java/62-implementing-generic-interface.java
+++ Exercises/10-Interfaces.java/62-implementing-generic-interface.java
@@ -2,12 +2,11 @@ public interface Comparable<T> { // <T> is saying that the Comparable interface 
   int compareTo(T o); // so notice when we declare the compareTo method, it has the same T, so we can specify in our implementation what type we want to expect to receive in that compareto method
 }
 
-public class Flight implements Comparable {
+public class Flight implements Comparable<Flight> { // We are now saying that we want to implement comparable using the Flight type
   // other members elided for clarity
   private int flightTime; // minutes past midnight
 
-  public int compareTo(Object o) {
-    Flight f = (Flight) o;
+  public int compareTo(Flight o) {
     return flightTime - f.flightTime;
   }
 }
