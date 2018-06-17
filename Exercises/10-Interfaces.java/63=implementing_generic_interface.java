11bffe51e5d785355146c5f288676ab2cecc035a
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-17 03:22:28 -0700

    added Passenger class again to showcase differences in previous and newly modified itertions

---
+++ Exercises/10-Interfaces.java/63=implementing_generic_interface.java
@@ -0,0 +1,20 @@
+public class Passenger implements Comparable {
+  // other members elided for clarity
+  private int memberLevel; // 3 (1st priority, think of platinum member), 2 (gold member), 1 (silver member)
+  private int memberDays;
+  public int compareTo(Object o) {
+    Passenger p = (Passenger) o;
+    if (memberLevel > p.memberLevel)
+      return -1;
+    else if (memberLevel < p.memberLevel)
+      return 1;
+    else {
+      if (memberDays > p.memberDays)
+        return -1;
+      else if (memberDays < p.memberDays)
+        return 1;
+      else
+        return 0;
+    }
+  }
+}
