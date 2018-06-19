37d5c528f942e5c7f07f59026b1f80287d6dd169
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-19 01:57:21 -0700

    working on new DynamicHelper class

---
+++ CalcEngine/11-CalcEngine_with_Interfaces/src/com.pluralsight/calcengine/DynamicHelper.java
@@ -0,0 +1,13 @@
+package com.pluralsight.calcengine;
+
+public class DynamicHelper {
+  private MathProcessing[] handlers;
+
+  public DynamicHelper(MathProcesing[] handlers) {
+    this.handlers = handlers;
+  }
+
+  public String process(String statement) {
+    
+  }
+}
