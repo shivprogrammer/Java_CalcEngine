1dc76f48c585cec330f6b70b9023173e7f54c270
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-18 23:48:53 -0700

    added in new private array variables to Flight and brought in Passenger & CrewMember classes

--- Exercises/10-Interfaces.java/64-implementing_multiple_interfaces.java
+++ Exercises/10-Interfaces.java/64-implementing_multiple_interfaces.java
@@ -1,9 +1,18 @@
 public class Flight implements Comparable(Flight) {
   // other members elided for clarity
   private int flightTime;
-
+  private CrewMember[] crew;
+  private Passenger[] roster;
   public int compareTo(Flight f) {
     Flight f = (Flight) o;
     return flightTime - f.flightTime;
   }
+}
+
+public class Passenger implements Comparable<Passenger> {
+  // members elided for clarity
+}
+
+public class CrewMember {
+  // members elided for clarity
 }
