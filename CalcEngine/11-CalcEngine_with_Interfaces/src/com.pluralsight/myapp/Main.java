4f61d281d384351e408526b59d1cfa4859afcfe0
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-22 00:24:06 -0500

    udpates to Main class using new DynamicHelper class

--- CalcEngine/11-CalcEngine_with_Interfaces/src/com.pluralsight/myapp/Main.java
+++ CalcEngine/11-CalcEngine_with_Interfaces/src/com.pluralsight/myapp/Main.java
@@ -19,8 +19,13 @@ public class Main {
       "add 25.0 92.0",          // 25.0 + 92.0 = 117.0
     };
 
+    DynamicHelper helper = new DynamicHelper(new MathProcessing[] {
+      new Adder()
+    });
+
     for (String statement : statements) {
-      
+      String output = helper.process(statement);
+      System.out.println(output);
     }
   }
 
