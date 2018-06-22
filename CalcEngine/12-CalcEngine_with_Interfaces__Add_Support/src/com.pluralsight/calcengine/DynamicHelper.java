b3260b842571e8ed196aa876b8921d64b2e36e45
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-22 00:19:14 -0500

    updates to process method in DynamicHelper clas

--- CalcEngine/12-CalcEngine_with_Interfaces__Add_Support/src/com.pluralsight/calcengine/DynamicHelper.java
+++ CalcEngine/12-CalcEngine_with_Interfaces__Add_Support/src/com.pluralsight/calcengine/DynamicHelper.java
@@ -10,5 +10,22 @@ public class DynamicHelper {
   public String process(String statement) {
     // IN: add 1.0 2.0
     // OUT: 1.0 + 2.0 = 3.0
+
+    String[] parts = statement.split(MathProcessing.SEPARATOR);
+    String keyword = parts[0]; // add
+
+    MathProcessing theHandler = null;
+
+    for (MathProcessing handler : handlers) {
+      if (keyword.equalsIgnoreCase(handler.getKeyword())) {
+        theHandler = handler;
+        break;
+      }
+    }
+
+    double leftVal = Double.parseDouble(parts[1]); // 1.0
+    double rightVal = Double.parseDouble(parts[2]); // 2.0
+
+    double result = theHandler.doCalculation(leftVal, rightVal);
   }
 }
