f206fc2b78880699d67d88e66df54238caa5799f
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-22 19:12:54 -0500

    utilizing the power of static members

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/65-static_members.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/65-static_members.java
@@ -20,8 +20,13 @@ class Flight {
 }
 
 Flight.resetAllPassengers();
-System.out.println(Flight.getAllPassengers()); // returns 0
+System.out.println(Flight.getAllPassengers()); // prints 0
 
 Flight lax045 = new Flight();
 lax045.add1Passenger(); // allPassengers == 1, lax045 passengers == 1
 lax034.add1Passenger(); // allPassengers = 2, lax045 passengers == 2
+
+Flight slc015 = new Flight();
+scl015.add1Passenger(); // allPassengers == 3, lax045 passengers == 2, slc015 passengers == 1 ------ Meaning that allPassengers is continuing to increment throughout all of the Flight instances, whereas the passengers count is associated only with a particular Flight instance
+
+System.out.println(Flight.getAllPassengers()); // prints 3
