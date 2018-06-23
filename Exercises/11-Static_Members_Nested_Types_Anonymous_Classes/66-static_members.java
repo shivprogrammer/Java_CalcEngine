31aad01eaf13538b2f8acaf6359ef58f03e2cada
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-22 19:17:07 -0500

    utilizing static imports for more succinct code

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/66-static_members.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/66-static_members.java
@@ -1,5 +1,8 @@
-Flight.resetAllPassengers();
-System.out.println(Flight.getAllPassengers());
+import static com.pluralsight.travel.Flight.resetAllPassengers;
+import static com.pluralsight.travel.Flight.getAllPassengers;
+
+resetAllPassengers();
+System.out.println(getAllPassengers());
 
 Flight lax045 = new Flight();
 lax045.add1Passenger();
@@ -8,4 +11,4 @@ lax034.add1Passenger();
 Flight slc015 = new Flight();
 scl015.add1Passenger();
 
-System.out.println(Flight.getAllPassengers());
+System.out.println(getAllPassengers());
