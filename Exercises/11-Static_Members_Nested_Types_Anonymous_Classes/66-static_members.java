f78b4bade1c402d82b993059cce5a69fd21a737c
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-22 19:15:30 -0500

    using static imports

---
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/66-static_members.java
@@ -0,0 +1,11 @@
+Flight.resetAllPassengers();
+System.out.println(Flight.getAllPassengers());
+
+Flight lax045 = new Flight();
+lax045.add1Passenger();
+lax034.add1Passenger();
+
+Flight slc015 = new Flight();
+scl015.add1Passenger();
+
+System.out.println(Flight.getAllPassengers());
