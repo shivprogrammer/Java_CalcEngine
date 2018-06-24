5fc0e391fb9c9a22dc0a680b852b0516b6f66114
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-23 23:19:09 -0700

    finished out CrewManager class

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/67-static_initialization_blocks.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/67-static_initialization_blocks.java
@@ -6,6 +6,14 @@ public class CrewManager {
   public static CrewMember
     FindAvailable (FlightCrewJob job) {
       CrewMember cm = null;
+      for (int i = 0; i < pool.length; i++) {
+        if (pool[i] != null && pool[i].job == job) {
+          cm = pool[i];
+          pool[i] = null;
+          break;
+        }
+      }
+      return cm;
     }
-
-}
+// other members temporarily elided
+} // class CrewManager
