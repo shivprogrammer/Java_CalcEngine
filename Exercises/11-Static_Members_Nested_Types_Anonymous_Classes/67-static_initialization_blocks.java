f5287c0e85f069e0ad8341b51fc20bae86a4b36b
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-23 23:13:38 -0700

    creating out CrewManager class

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/67-static_initialization_blocks.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/67-static_initialization_blocks.java
@@ -1,1 +1,11 @@
 CrewMember p = CrewManager.FindAvailable(FlightCrewJob.Pilot);
+
+public class CrewManager {
+  private final static String FILENAME = "...";
+  public static CrewMember[] pool;
+  public static CrewMember
+    FindAvailable (FlightCrewJob job) {
+      CrewMember cm = null;
+    }
+
+}
