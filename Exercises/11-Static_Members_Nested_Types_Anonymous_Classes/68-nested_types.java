af8001278c325a9cc427e5d68b762137611566db
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-24 22:46:42 -0500

    finishing out RewardProgram nested class within Passenger enclosing class

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/68-nested_types.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/68-nested_types.java
@@ -3,5 +3,12 @@ public class Passenger implements Comparable { // Passenger is the enclosing cla
   public static class RewardProgram { // this is the nested class
     private int memberLevel;
     private int memberDays;
+
+    public int getLevel() { return level; }
+    public void setLevel(int level) { this.level = level; }
+
+    public int getMemberDays() { return memberDays; }
+    public void setMemberDays(int days) { this.memberDays = days; }
   }
+  private RewardProgram rewardProgram = new RewardProgram();
 }
