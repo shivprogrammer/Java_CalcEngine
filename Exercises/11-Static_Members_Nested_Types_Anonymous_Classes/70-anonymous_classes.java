27d7ffe7155d70d44f284ca1f73ccf633c43c000
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-26 15:21:05 -0700

    made FlightIterator an anonymous class within the Flight class

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/70-anonymous_classes.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/70-anonymous_classes.java
@@ -3,17 +3,16 @@ public class Flight implements Comparable<Flight>, Iterable(Person) {
   private CrewMember[] crew;
   private Passenger[] roster;
   public Iterator<Person> iterator() {
-    return new FlightIterator(crew, roster);
-  }
-  private class FlightIterator implements Iterator<Person> {
-    private int index = 0;
-    public boolean hasNext() {
-      return index < (crew.length + roster.length);
-    }
-    public Person next() {
-      Person p = (index < crew.length) ? crew[index] : roster[index - crew.length];
-      index++;
-      return p;
+    return new Iterator<Person>() {
+      private int index = 0;
+      public boolean hasNext() {
+        return index < (crew.length + roster.length);
+      }
+      public Person next() {
+        Person p = (index < crew.length) ? crew[index] : roster[index - crew.length];
+        index++;
+        return p;
+      }
     }
   }
 }
