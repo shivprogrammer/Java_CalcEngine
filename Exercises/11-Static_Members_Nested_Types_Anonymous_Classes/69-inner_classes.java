57e9225812059efc99cdf25d1041fbb40e25073e
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-26 15:13:49 -0700

    removed separate FlightIterator, now only using inner class of FlightIterator

--- Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/69-inner_classes.java
+++ Exercises/11-Static_Members_Nested_Types_Anonymous_Classes/69-inner_classes.java
@@ -17,21 +17,3 @@ public class Flight implements Comparable<Flight>, Iterable(Person) {
     }
   }
 }
-
-public class FlightIterator implements Iterator<Person> {
-  private CrewMember[] crew;
-  private Passenger[] roster;
-  private int index = 0;
-  public FlightIterator(CrewMember[] crew, Passenger[] roster) {
-    this.crew = crew;
-    this.roster = roster;
-  }
-  public boolean hasNext() {
-    return index < (crew.length + roster.length);
-  }
-  public Person next() {
-    Person p = (index < crew.length) ? crew[index] : roster[index - crew.length];
-    index++;
-    return p;
-  }
-}
